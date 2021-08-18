ee6538dd048a5f433a07a90d0d308cefee4ee5d1
Luis1604 <luisbravo1804@gmail.com>
2021-08-17 22:39:04 -0500

    Actualizacion modelo

--- HotelWeb/settings.py
+++ HotelWeb/settings.py
@@ -121,13 +121,6 @@ USE_TZ = True
 
 STATIC_URL = '/templates/'
 
-STATICFILES_DIRS = (
-    # Put strings here, like "/home/html/static" or "C:/www/Django/static".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-    os.path.join(os.path.dirname(__file__),'templates').replace('\\','/'),
-)
-
 
 
 # Default primary key field type
