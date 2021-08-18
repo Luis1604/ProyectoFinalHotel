a173570e8603e088d786ae96cdda4dd36697157a
Xavier Villavicencio <jxavier2525@gmail.com>
2021-08-18 13:39:01 -0500

    get y post

--- Hotel/serializers.py
+++ Hotel/serializers.py
@@ -5,45 +5,45 @@ from .models import Persona, Rol, Habitacion, Reserva, RegistroHuespedes, Servic
 class rolSerializer(serializers.ModelSerializer):
     class Meta:
         model = Rol
-        fields = ['id', 'Administrador', 'Cliente']
+        fields = ['Administrador', 'Cliente']
 
 
 class personaSerializer(serializers.ModelSerializer):
     class Meta:
         model = Persona
-        fields = ['id', 'Rol', 'cedula', 'Nombre', 'Apellido',
+        fields = ['Rol', 'cedula', 'Nombre', 'Apellido',
                   'Email', 'Telefono', 'Direccion', 'password']
 
 
 class habitacionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Habitacion
-        fields = ['id', 'numHabitacion', 'Tipo',
+        fields = ['numHabitacion', 'Tipo',
                   'numPiso', 'Detalle', 'Precio', 'Estado']
 
 
 class reservaSerializer(serializers.ModelSerializer):
     class Meta:
         model = Reserva
-        fields = ['id', 'Persona', 'numReserva',
+        fields = ['Persona', 'numReserva',
                   'numHabitacion', 'Fecha_Ingreso', 'Fecha_Caducidad']
 
 
 class registroHuespedesSerializer(serializers.ModelSerializer):
     class Meta:
         model = RegistroHuespedes
-        fields = ['id', 'Reserva', 'numPersonas',
+        fields = ['Reserva', 'numPersonas',
                   'Fecha_Llegada', 'Fecha_Salida', 'Estado']
 
 
 class serviciosSerializer(serializers.ModelSerializer):
     class Meta:
         model = Servicios
-        fields = ['id', 'Nombre_Servicio', 'Precio', 'Descripcion']
+        fields = ['Nombre_Servicio', 'Precio', 'Descripcion']
 
 
 class pagoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Pago
-        fields = ['id', 'Habitacion', 'Servicios', 'numPago', 'cedula',
+        fields = ['Habitacion', 'Servicios', 'numPago', 'cedula',
                   'Nombre', 'Apellido', 'Email', 'Direccion', 'Detalle', 'Total', 'Estado']
--- Hotel/views.py
+++ Hotel/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render, HttpResponse
 from django.shortcuts import render, redirect
+from rest_framework import serializers
 from .models import Rol, Persona, Habitacion, RegistroHuespedes, Reserva, Servicios, Pago
-from django.views.decorators.csrf import csrf_protect
+from django.views.decorators.csrf import csrf_exempt
 from .forms import FormPago, FormPersona, FormReserva
+from .serializers import personaSerializer
+from django.http import JsonResponse
+from rest_framework.parsers import JSONParser
 # Create your views here.
 
 
@@ -10,17 +14,33 @@ def index(request):
     return HttpResponse("Funcionando")
 
 
+@csrf_exempt
 def registrar(request):
-    return HttpResponse("registrar")
+    if request.method == 'GET':
+        persona = Persona.objects.all()
+        serializer = personaSerializer(persona, many=True)
+        return JsonResponse(serializer.data, safe=False)
+
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = personaSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
+
 
 def login_view(request):
     return render(request, "login.html", {})
 
+
 def create_view(request):
     return render(request, "Create.html", {})
 
+
 def inicio_view(request):
     return render(request, "inicio.html", {})
+
 
 def reserva_view(request):
     return render(request, "reserva.html", {})
