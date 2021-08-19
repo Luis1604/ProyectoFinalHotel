d75978703d9c4a7e86c36d77e36552132c540706
Xavier Villavicencio <jxavier2525@gmail.com>
2021-08-19 00:27:11 -0500

    Cambios view

--- Hotel/urls.py
+++ Hotel/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
-from .views import registrar_list, login_view, inicio_view, reserva_view, servicio_view, registrar_detail
+from .views import login_view, inicio_view, reserva_view, servicio_view, registrarList, registrarDetail
+# registrar_list, registrar_detail
 
 urlpatterns = [
     path('', inicio_view, name='Login'),
-    path('register/', registrar_list),
-    path('register/<int:pk>/', registrar_detail),
+    #path('register/', registrar_list),
+    #path('register/<int:pk>/', registrar_detail),
+    path('register/', registrarList.as_view()),
+    path('register/<int:pk>/', registrarDetail.as_view()),
     path('home/', inicio_view),
     path('reserva/', reserva_view),
     path('servicios/', servicio_view),
